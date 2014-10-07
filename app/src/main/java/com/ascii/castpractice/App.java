887e963264acd9c4ff3d5091b8ffbe8751f69f5b
Ascii <asciihuang@kkbox.com>
2014-10-07 15:00:23 +0800

    test

--- app/src/main/java/com/ascii/castpractice/App.java
+++ app/src/main/java/com/ascii/castpractice/App.java
@@ -3,13 +3,10 @@ package com.ascii.castpractice;
 import android.app.Application;
 
 public class App extends Application {
-
 	@Override
 	public void onCreate() {
 		super.onCreate();
 		Cast = new ChromecastController(this);
 	}
-
 	public ChromecastController Cast;
-
 }
